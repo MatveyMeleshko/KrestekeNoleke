af5fbe6abd15a273dc24dd0d3069e134db512fbf
User <User@WIN-3CR465786FQ>
2024-03-11 17:44:39 +0300

    Добавьте файлы проекта.

---
+++ КрестикиНолики/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace КрестикиНолики
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.playToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newPlayToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clouseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.playToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(360, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // playToolStripMenuItem
+            // 
+            this.playToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newPlayToolStripMenuItem,
+            this.clouseToolStripMenuItem});
+            this.playToolStripMenuItem.Name = "playToolStripMenuItem";
+            this.playToolStripMenuItem.Size = new System.Drawing.Size(60, 24);
+            this.playToolStripMenuItem.Text = "Menu";
+            this.playToolStripMenuItem.Click += new System.EventHandler(this.playToolStripMenuItem_Click);
+            // 
+            // newPlayToolStripMenuItem
+            // 
+            this.newPlayToolStripMenuItem.Name = "newPlayToolStripMenuItem";
+            this.newPlayToolStripMenuItem.Size = new System.Drawing.Size(154, 26);
+            this.newPlayToolStripMenuItem.Text = "New play";
+            this.newPlayToolStripMenuItem.Click += new System.EventHandler(this.newPlayToolStripMenuItem_Click);
+            // 
+            // clouseToolStripMenuItem
+            // 
+            this.clouseToolStripMenuItem.Name = "clouseToolStripMenuItem";
+            this.clouseToolStripMenuItem.Size = new System.Drawing.Size(154, 26);
+            this.clouseToolStripMenuItem.Text = "Close";
+            this.clouseToolStripMenuItem.Click += new System.EventHandler(this.clouseToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei UI", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(12, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 59);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.ElementsClick);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft YaHei UI", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(123, 43);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 59);
+            this.button2.TabIndex = 11;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.ElementsClick);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft YaHei UI", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(235, 43);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(106, 59);
+            this.button3.TabIndex = 12;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.ElementsClick);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft YaHei UI", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(236, 108);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(106, 59);
+            this.button4.TabIndex = 15;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.ElementsClick);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft YaHei UI", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button5.Location = new System.Drawing.Point(124, 108);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(106, 59);
+            this.button5.TabIndex = 14;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.ElementsClick);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft YaHei UI", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button6.Location = new System.Drawing.Point(12, 108);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(106, 59);
+            this.button6.TabIndex = 13;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.ElementsClick);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft YaHei UI", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button7.Location = new System.Drawing.Point(236, 173);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(106, 59);
+            this.button7.TabIndex = 18;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.ElementsClick);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft YaHei UI", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button8.Location = new System.Drawing.Point(124, 173);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(106, 59);
+            this.button8.TabIndex = 17;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.ElementsClick);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Microsoft YaHei UI", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button9.Location = new System.Drawing.Point(12, 173);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(106, 59);
+            this.button9.TabIndex = 16;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.ElementsClick);
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button10.Location = new System.Drawing.Point(13, 109);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(329, 59);
+            this.button10.TabIndex = 19;
+            this.button10.Text = "Play";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 240);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Крестики-нолики";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem playToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newPlayToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clouseToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+    }
+}
+
---
+++ КрестикиНолики/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace КрестикиНолики
+{
+    public partial class Form1 : Form
+    {
+        System.Windows.Forms.Button[,] field;
+        string player;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void playToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void IsField()
+        {
+            player = "X";
+            field = new System.Windows.Forms.Button[3, 3];
+
+
+            field[0, 0] = button1;
+            field[0, 1] = button2;
+            field[0, 2] = button3;
+
+            field[1, 0] = button4;
+            field[1, 1] = button5;
+            field[1, 2] = button6;
+
+            field[2, 0] = button7;
+            field[2, 1] = button8;
+            field[2, 2] = button9;
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    field[i, j].Text = " ";
+                }
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            button10.Visible = false;
+            IsField();
+        }
+
+        private bool IsWin()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (field[i, 0].Text == field[i, 1].Text && field[i, 1].Text == field[i, 2].Text && field[i, 0].Text != " ")
+                {
+                    return true;
+                }
+            }
+
+            if (field[0, 0].Text == field[1, 1].Text && field[1, 1].Text == field[2, 2].Text && field[0, 0].Text != " ")
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool IsFree()
+        {
+            int coun = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (field[i, j].Text == " ")
+                    {
+                        coun++;
+                    }
+                }
+            }
+
+            if (coun > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void ElementsClick(object sender, EventArgs e)
+        {
+            Player(sender);
+
+            SuperiorityBot();
+        }
+
+        private bool Player(object sender)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (((System.Windows.Forms.Button)sender).Equals(field[i, j]))
+                    {
+                        if (field[i, j].Text == " ")
+                        {
+                            field[i, j].Text = player;
+                            if (IsWin())
+                            {
+                                MessageBox.Show($"Победил игрок");
+                                IsField();
+                                return true;
+                            }
+                            else if (IsFree() == false)
+                            {
+                                MessageBox.Show($"Победил SuperiorityBot");
+                                IsField();
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        private void SuperiorityBot()
+        {
+            Random random = new Random();
+            int x = random.Next(0, 3);
+            int y = random.Next(0, 3);
+
+            if (field[1, 1].Text == " ")
+            {
+                field[1, 1].Text = "O";
+            }
+            else if (field[0, 2].Text == " ")
+            {
+                field[0, 2].Text = "O";
+            }
+            else
+            {
+                field[x, y].Text = "O";
+            }
+        }
+
+        private void newPlayToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            IsField();
+        }
+        private void clouseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
